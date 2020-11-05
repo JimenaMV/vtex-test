b41a212ff8106f5d7ebc33e3135cbbbeb950e0fc
USER <user@USERs-iMac.local>
2020-11-04 23:52:42 -0600

    Code commented

--- src/App.js
+++ src/App.js
@@ -2,31 +2,36 @@ import React, { useState } from "react";
 
 function App() {
   //State
-  const [permutation, setPermutation] = useState([]);
+  //const [permutation, setPermutation] = useState([]);
 
   const permutations = (inputArray) => {
     //Array vacío donde se guardarán las permutaciones
     const arrayPermutation = [];
 
-    //
-
+    //permute hace cada iteración, recibe dos parámetros.En permutation = [] es donde se va a almacenar un a de las posibles permutaciones
     const permute = (arr, permutation = []) => {
+      //Se deja de ejecutar cuando el array es cero
       if (!arr.length) {
         arrayPermutation.push(permutation);
         return;
       }
 
       for (let i in arr) {
+        //En current se almacena un nuevo array con slice para no modificar el original
         let current = arr.slice();
+        //Con slice se van quitando las letras del current y crea un array nuevo con ese valor
         let next = current.splice(i, 1);
         permute(current, permutation.concat(next));
       }
     };
+    //Ejecutando la función permute
     permute(inputArray);
     return arrayPermutation;
   };
 
-  /*   if (permutation === []) {
+  /* setPermutation(permutations(["A", "B", "C"])); */
+
+  /* if (permutation === []) {
     setPermutation(permutations(["A", "B", "C"]));
   } */
   console.log(permutations(["A", "B", "C"]));
@@ -41,13 +46,11 @@ function App() {
         ))}
       </ul>
 
-      {/* <ul>
+      {/*   <ul>
         {permutation.map((element, index) => (
           <li key={index}>{element}</li>
         ))}
       </ul> */}
-
-      {/* <p>{permutations(["A", "B", "C"])}</p> */}
     </>
   );
 }
