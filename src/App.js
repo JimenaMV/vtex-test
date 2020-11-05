36797e90a3301b70c69e19798542cea679016d42
USER <user@USERs-iMac.local>
2020-11-04 21:19:12 -0600

    Code Uptate

--- src/App.js
+++ src/App.js
@@ -4,14 +4,11 @@ function App() {
   //State
   const [permutation, setPermutation] = useState([]);
 
-  /* const a = ["A", "B", "C"]; */
-
   const permutations = (inputArray) => {
-    //1. Declarar variable  con array vacío donde se giardarán las permutaciones
+    //Array vacío donde se guardarán las permutaciones
     const arrayPermutation = [];
 
-    //2. Iterar con un for sobre la longitud del array
-    //3. Declarar variable en donde haga slice y se guarde la primera letra y se una conla segunda(Puedo usar ...?)
+    //
 
     const permute = (arr, permutation = []) => {
       if (!arr.length) {
@@ -20,36 +17,35 @@ function App() {
       }
 
       for (let i in arr) {
-        let curr = arr.slice();
-        let next = curr.splice(i, 1);
-        permute(curr, permutation.concat(next));
+        let current = arr.slice();
+        let next = current.splice(i, 1);
+        permute(current, permutation.concat(next));
       }
     };
     permute(inputArray);
     return arrayPermutation;
   };
 
-  /* console.log(permutations(["A", "B", "C"])); */
-
-  if (permutation === []) {
+  /*   if (permutation === []) {
     setPermutation(permutations(["A", "B", "C"]));
-  }
+  } */
+  console.log(permutations(["A", "B", "C"]));
 
   return (
     <>
       <h1>VETEX</h1>
       <p>Permutaciones de tres elementos</p>
-      {/* <ul>
+      <ul>
         {permutations(["A", "B", "C"]).map((element, index) => (
           <li key={index}>{element}</li>
         ))}
-      </ul> */}
-
-      <ul>
-        {permutation.map((element, index) => (
-          <li key={index}>{element.permutation}</li>
-        ))}
       </ul>
+
+      {/* <ul>
+        {permutation.map((element, index) => (
+          <li key={index}>{element}</li>
+        ))}
+      </ul> */}
 
       {/* <p>{permutations(["A", "B", "C"])}</p> */}
     </>
